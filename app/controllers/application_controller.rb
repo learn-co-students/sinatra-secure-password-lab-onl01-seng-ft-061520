771fe0864b8acf7413b86831525bc675d583cb01
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-29 16:14:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
       User.create(username: params[:username], password: params[:password])
       redirect '/login'
     end
-
+    
   end
 
   get "/account" do
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20200729154956) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
 end
